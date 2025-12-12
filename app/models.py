3530f145e3552c9f4fc545be11b7efa3c265517c
GSofoyan <Gevorg.Sofoyan@UGent.be>
2025-12-12 16:26:39 +0100

    Update models.py

--- app/models.py
+++ app/models.py
@@ -15,15 +15,15 @@ gebruikersrol_enum = ENUM('Admin', 'Key user', 'User', name='gebruikersrol', cre
 # MODELS
 # ============================
 
-class Productiebedrijf(db.Model):
-    __tablename__ = 'productiebedrijf'
-    productiebedrijf_id = db.Column(db.Integer, primary_key=True)
+class Businessunit(db.Model):
+    __tablename__ = 'businessunit'
+    businessunit_id = db.Column(db.Integer, primary_key=True)
     naam = db.Column(db.String(255), nullable=False)
     adres = db.Column(db.String(255))
     contactpersoon = db.Column(db.String(255))
 
     # Relatie naar vertegenwoordigers die bij dit productiebedrijf horen
-    vertegenwoordigers = db.relationship('Gebruiker', backref='productiebedrijf', lazy=True)
+    vertegenwoordigers = db.relationship('Gebruiker', backref='businessunit', lazy=True)
 
 class Gebruiker(db.Model):
     __tablename__ = 'gebruiker'
@@ -33,7 +33,7 @@ class Gebruiker(db.Model):
     rol = db.Column(gebruikersrol_enum, nullable=False)
     wachtwoord = db.Column(db.Text, nullable=False)
     telefoon = telefoon = db.Column(db.Text)
-    productiebedrijf_id = db.Column(db.Integer, db.ForeignKey('productiebedrijf.productiebedrijf_id', onupdate='CASCADE'))
+    businessunit_id = db.Column(db.Integer, db.ForeignKey('businessunit.businessunit_id', onupdate='CASCADE'))
 
 
     # Relatie naar statushistoriek die door deze gebruiker is gewijzigd
@@ -50,6 +50,7 @@ class Klant(db.Model):
     email = db.Column(db.Text)
     telefoon =  db.Column(db.Text)
     adres = db.Column(db.Text)
+    ondernemingsnummer = db.Column(db.Integer)
 
     # Relaties naar orders en klachten
     orders = db.relationship('Order', backref='klant', lazy=True)
@@ -57,10 +58,11 @@ class Klant(db.Model):
 
 class Order(db.Model):
     __tablename__ = 'order'
-    order_id = db.Column(db.Integer, primary_key=True)
+    order_nummer = db.Column(db.String(255), primary_key=True)
     created_at = db.Column(db.DateTime(timezone=True), nullable=False, default=datetime.utcnow)
     klant_id = db.Column(db.Integer, db.ForeignKey('klant.klant_id'))
 
+    producten = db.relationship('Product', secondary='producten_order',backref=db.backref('orders_via_m2m', lazy='dynamic'),lazy='dynamic')
 
 class Probleemcategorie(db.Model):
     __tablename__ = 'probleemcategorie'
@@ -73,19 +75,21 @@ class Probleemcategorie(db.Model):
 class Klacht(db.Model):
     __tablename__ = 'klacht'
     klacht_id = db.Column(db.Integer, primary_key=True)
-    vertegenwoordiger_id = db.Column(db.Integer, db.ForeignKey('gebruiker.gebruiker_id', ondelete='SET NULL'))
+    verantwoordelijke_id = db.Column(db.Integer, db.ForeignKey('gebruiker.gebruiker_id', ondelete='SET NULL'))
     klant_id = db.Column(db.Integer, db.ForeignKey('klant.klant_id', ondelete='SET NULL'))
     categorie_id = db.Column(db.Integer, db.ForeignKey('probleemcategorie.categorie_id', ondelete='SET NULL'))
-    order_nummer = db.Column(db.String(255), nullable=False)
+    order_nummer = db.Column(db.String(255), db.ForeignKey('order.order_nummer', ondelete='SET NULL'))
     mogelijke_oorzaak = db.Column(db.Text)
-    bijlages = db.Column(JSONB)
+    bijlages = db.Column(db.Text)
     prioriteit = db.Column(db.Boolean, default=False)
     status = db.Column(klacht_status_enum, default='Ingediend')
     datum_melding = db.Column(db.DateTime(timezone=True), default=datetime.utcnow)
-    reden_afwijzing = db.Column(db.Text)
-    gm_opmerking = db.Column(db.Text)
+    klacht_omschrijving = db.Column(db.Text)
+    opmerking_status_wijziging = db.Column(db.Text)
     datum_laatst_bewerkt = db.Column(db.DateTime(timezone=True), default=datetime.utcnow)
-    
+    businessunit_id = db.Column(db.Integer, db.ForeignKey('businessunit.businessunit_id', onupdate='CASCADE'))
+    aantal_eenheden = db.Column(db.Integer)
+    artikelnummer = db.Column(db.Integer, db.ForeignKey('product.artikel_nr', ondelete='SET NULL'))
 
     # Relatie naar statushistoriek
     statushistoriek = db.relationship('StatusHistoriek', backref='klacht', lazy=True, cascade='all, delete')
@@ -99,3 +103,22 @@ class StatusHistoriek(db.Model):
     gewijzigd_door = db.Column(db.Integer, db.ForeignKey('gebruiker.gebruiker_id'))
     opmerking = db.Column(db.Text)
     datum_wijziging = db.Column(db.DateTime(timezone=True), default=datetime.utcnow)
+
+class Product(db.Model):
+    __tablename__ = 'product'
+    artikel_nr = db.Column(db.Integer, primary_key=True)
+    naam = db.Column(db.String(255), nullable=False)
+
+    orders = db.relationship('Order', secondary='producten_order', backref=db.backref('producten_via_m2m', lazy='dynamic'),lazy='dynamic')
+
+class Producten_Order(db.Model):
+    __tablename__ = 'producten_order'
+    order_nummer = db.Column(db.String(255), db.ForeignKey('order.order_nummer'), primary_key=True,nullable=False)
+    artikel_nr = db.Column(db.Integer, db.ForeignKey('product.artikel_nr'),primary_key=True,nullable=False)
+    aantal = db.Column(db.Integer, nullable=False)
+
+    order = db.relationship('Order', backref='product_orders', lazy=True)
+    product = db.relationship('Product', backref='order_links', lazy=True)
+
+
+    
